b25ff6d3e0c2a1f76608a93d536c798cf0c06e1f
ernesto <ernesto002@icloud.com>
2021-09-26 12:57:32 -0700

    render park data onto page

--- src/components/Park.js
+++ src/components/Park.js
@@ -1,3 +1,32 @@
 class Park {
-    
+
+    static all = []
+    constructor(data){
+        this.data = data
+        this.constructor.all.push(this)
+    }
+
+    renderCard = () => {
+        const { name, address, city, state, detail, imageUrl, id } = this.data
+        document.querySelector(".park-container").innerHTML += `
+        <div class="park-card">
+            <img src=${imageUrl} alt=${name}/>
+            <p class="title">${name}</p>
+            <p>${city}, ${state}</p>
+        </div>`
+    }
+
+    static renderIndex(){
+        const parkContainer = document.createElement("div")
+        parkContainer.classList.add("park-container")
+        document.getElementById("main").appendChild(parkContainer)
+        this.all.forEach(park => park.renderCard())
+    }
+
+    static getParks(){
+        api.getParks().then(parks => {
+            parks.forEach(park => new Park(park))
+            this.renderIndex()
+        })
+    }
 }
