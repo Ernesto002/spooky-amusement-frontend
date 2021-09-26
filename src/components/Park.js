215bf8bae6c5a72244e7684c1dc4b57e4f664fba
ernesto <ernesto002@icloud.com>
2021-09-26 13:35:33 -0700

    add show page for park/add click event listener that gets id of specific park/add go back button

--- src/components/Park.js
+++ src/components/Park.js
@@ -6,24 +6,53 @@ class Park {
         this.constructor.all.push(this)
     }
 
+    renderShow = () => {
+        const { name, address, city, state, detail, imageUrl } = this.data
+        document.getElementById("main").innerHTML = `
+        <div class="show">
+            <h1>${name}</h1>
+            <img src=${imageUrl} alt=${name}/>
+            <p>${address}</p>
+            <p>${city}, ${state}</p>
+            <p>${detail}</p>
+        </div>
+        <button id="goBack">Go Back</button>
+        `
+        document.getElementById("goBack").addEventListener("click", Park.renderIndex)
+    }
+
     renderCard = () => {
-        const { name, address, city, state, detail, imageUrl, id } = this.data
-        document.querySelector(".park-container").innerHTML += `
-        <div class="park-card">
+        const { name, city, state, imageUrl, id} = this.data
+        document.getElementById("park-container").innerHTML += `
+        <div class="park-card" data-id=${id}>
             <img src=${imageUrl} alt=${name}/>
             <p class="title">${name}</p>
             <p>${city}, ${state}</p>
         </div>`
     }
 
-    static renderIndex(){
-        const parkContainer = document.createElement("div")
-        parkContainer.classList.add("park-container")
-        document.getElementById("main").appendChild(parkContainer)
-        this.all.forEach(park => park.renderCard())
+
+    static find = (id) => this.all.find(park => park.data.id == id)
+
+
+    static handleIndexClick = (e) => {
+        if (e.target.tagName == "IMG" || e.target.classList.contains("title")){
+            const id = e.target.closest(".park-card").dataset.id
+            this.find(id).renderShow()
+        }
     }
 
-    static getParks(){
+    static renderIndex = () => {
+        main.innerHTML = ` `
+        main.appendChild(parkContainer)
+        const parkContainer = document.createElement("div")
+        parkContainer.id = "park-container"
+        document.getElementById("main").appendChild(parkContainer)
+        this.all.forEach(park => park.renderCard())
+        parkContainer.addEventListener("click", this.handleIndexClick)
+    }
+
+    static getParks = () => {
         api.getParks().then(parks => {
             parks.forEach(park => new Park(park))
             this.renderIndex()
