fb121ce53853ed371a4cf27e5891bac41ae13610
ernesto <ernesto002@icloud.com>
2021-09-29 13:13:07 -0700

    add user form/persist new user creation to backend

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 const api = new ApiService("http://localhost:3000")
 const modal = new Modal()
 
-Park.getParks()
+// Park.getParks()
+document.querySelector("form").addEventListener("submit", handleUsernameSubmit)
+
+function handleUsernameSubmit(e){
+  e.preventDefault()
+  api.findOrCreateUser(e.target.username.value).then(console.log)
+}
--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,19 +1,31 @@
 class ApiService {
 
-    constructor(api){
-        this.api = api
-    }
+  constructor(api){
+    this.api = api
+  }
 
-    getParks = () => fetch(this.api + "/parks").then(res => res.json())
+  getParks = () => fetch(this.api + "/parks").then(res => res.json())
 
-    createPark = (newPark) => {
-        return fetch(this.api + "/parks", {
-          method: 'POST', // or 'PUT'
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(newPark),
-        })
-        .then(response => response.json())
-      }
+  createPark = (newPark) => {
+    return fetch(this.api + "/parks", {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newPark),
+    })
+    .then(response => response.json())
+  }
+
+  findOrCreateUser = (username) => {
+    return fetch(this.api + "/users", {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({username: username}),
+    })
+    .then(response => response.json())
+  }
+  
 }
